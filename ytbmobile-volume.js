ce778e0defd04f2dab09823f6c1a5828c1eb90a7
99NguyenChiThanh <71547990+99NguyenChiThanh@users.noreply.github.com>
2025-02-12 23:37:08 +0700

    Delete ytbmobile-volume.js

--- ytbmobile-volume.js
+++
@@ -1,9 +0,0 @@
-ytbmobile-volume.js text/javascript
-(function(){
-  window.__volumeForced=true;
-  let forceVolume=() => {
-    document.getElementsByTagName('video')[0].volume=0.2;
-    requestAnimationFrame(forceVolume);
-  };
-  forceVolume();
-})();
