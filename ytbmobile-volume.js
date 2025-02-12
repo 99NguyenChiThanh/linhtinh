9caf1146cb9880bb2e82a1da9a68bbad812c9e8d
99NguyenChiThanh <71547990+99NguyenChiThanh@users.noreply.github.com>
2025-02-12 23:26:50 +0700

    Update ytbmobile-volume.js

--- ytbmobile-volume.js
+++ ytbmobile-volume.js
@@ -1,8 +1,5 @@
 ytbmobile-volume.js text/javascript
 (function(){
-  document.write(5 + 6);
-  if(!!window.__volumeForced)
-    return;
   window.__volumeForced=true;
   let forceVolume=() => {
     document.getElementsByTagName('video')[0].volume=0.2;
